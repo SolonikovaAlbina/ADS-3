8df089f139cbe8f4a666b0618e39dd24169e6cd3
SolonikovaAlbina <99264411+SolonikovaAlbina@users.noreply.github.com>
2022-03-01 21:00:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int right = 0, left = size - 1, sum = 0;
+  while (right < left) {
+    int middle = (right + left) / 2;
+    if (arr[middle] < value)
+      right = middle + 1;
+    else
+      left = middle;
+  }
+  if (arr[right] == value) {
+    while (arr[right] == value) {
+      sum++;
+      right++;
+    }
+  }
+  if (sum != 0)
+    return sum;
+  else
+    return 0; // если ничего не найдено
 }
